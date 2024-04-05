ea24241ebeb29572262bdc0af988a8831c8112dd
habibrifqi <48439343+habibrifqi@users.noreply.github.com>
2024-04-05 20:55:01 +0700

    setup redux

---
+++ src/redux/boardsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+import data from '../data/data.json'
+
+const boardsSlice = createSlice({
+    name: 'boards',
+    initialState: {
+        boards: data.boards
+    },
+    reducers:{
+
+    }
+//     reducers: {
+//         addBoard: (state, action) => {
+//             state.boards.push(action.payload)
+//         },
+//         deleteBoard: (state, action) => {
+//             state.boards = state.boards.filter((board) => board.name !== action.payload)
+// }
+    })
+
+export default boardsSlice 
